001cfe179a6638f95c0da4629e8ab98ff31aaf47
Ujjwal Katiyar <ukcode07@gmail.com>
2025-11-30 00:54:12 +0530

    add HttpCodes: define HTTP status codes and their corresponding numeric values

---
+++ src/lib/HttpCodes.ts
@@ -0,0 +1,25 @@
+interface ICodes {
+  OK: number;
+  CREATED: number;
+  BAD_REQUEST: number;
+  UNAUTHORIZED: number;
+  FORBIDDEN: number;
+  NOT_FOUND: number;
+  CONFLICT: number;
+  UNPROCESSABLE_CONTENT: number;
+  TOO_MANY_REQUESTS: number;
+  INTERNAL_SERVER_ERROR: number;
+}
+
+export const codes: ICodes = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  UNPROCESSABLE_CONTENT: 422,
+  TOO_MANY_REQUESTS: 429,
+  INTERNAL_SERVER_ERROR: 500,
+};
