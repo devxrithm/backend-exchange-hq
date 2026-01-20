a1a70a4c31bf1eac6b0628e90906bf3242db745e
Ujjwal Katiyar <ukcode07@gmail.com>
2026-01-20 13:20:02 +0530

    fix: correct import path for dbConnect and update function name to startServer in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import { app } from "./app";
-import dbConnect from "./config/dbConnection";
+import dbConnect from "./config/db-config/db-connection";
 
-const StartServer = async () => {
+const startServer = async () => {
   await dbConnect();
   app.listen(3000, () => {
     console.log("server running on port 3000");
   });
 };
 
-StartServer();
+startServer();
