2f79956b6c250e4865325cdcfdda69e58916727a
Ujjwal Katiyar <ukcode07@gmail.com>
2026-01-20 20:19:51 +0530

    chore: remove console logs related to topic creation in Kafka initialization

--- src/config/kafka-config/kafka-initaliazation.ts
+++ src/config/kafka-config/kafka-initaliazation.ts
@@ -2,9 +2,7 @@ import KafkaConfig from "./kafka-config";
 
 export const initKafkaService = async () => {
   try {
-    console.log("topic creation in progress");
     await KafkaConfig.connect();
-    console.log("topic creation is success");
   } catch (error) {
     console.log(error);
     process.exit(1);
